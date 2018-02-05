23bddd1e8480b63fb0fbdb586c80cea3c8670f01
jMyles <justin@justinholmes.com>
2018-02-04 16:23:42 -0800

    Using hey_joe for example code so that it works in python3.

--- run.py
+++ run.py
@@ -1,29 +1,35 @@
+import sys
+
+from autobahn.twisted.websocket import WebSocketServerFactory
 from hendrix.contrib.async.messaging import hxdispatcher
 from hendrix.deploy.base import HendrixDeploy
-
-from txsockjs.factory import SockJSResource
-from hendrix.contrib.async.resources import MessageHandlerProtocol
+from hendrix.experience import hey_joe
 from hendrix.facilities.resources import NamedResource
-from twisted.internet.protocol import Factory
+from twisted.python import log
 
 message_resource = NamedResource('hendrix-demo')
-message_resource.putChild('messages', SockJSResource(Factory.forProtocol(MessageHandlerProtocol)))
-
-
 
 from twisted.conch.telnet import TelnetTransport, TelnetProtocol
 from twisted.internet.protocol import ServerFactory
 
 
 class TelnetToWebsocket(TelnetProtocol):
-
     def dataReceived(self, data):
         hxdispatcher.send('noodly_messages', data)
+
 
 telnet_server_factory = ServerFactory()
 telnet_server_factory.protocol = lambda: TelnetTransport(TelnetToWebsocket)
 
 deployer = HendrixDeploy(options={'wsgi': 'hendrix_demo.wsgi.application'})
 deployer.resources.append(message_resource)
+
+log.startLogging(sys.stdout)
+
+factory = WebSocketServerFactory(u"ws://127.0.0.1:9000")
+factory.protocol = hey_joe.MyServerProtocol
+
+deployer.reactor.listenTCP(9000, factory)
+
 deployer.reactor.listenTCP(6565, telnet_server_factory)
 deployer.run()
